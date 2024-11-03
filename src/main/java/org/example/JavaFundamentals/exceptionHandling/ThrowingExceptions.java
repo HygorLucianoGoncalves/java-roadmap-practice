fd8f8675393c052585db050ce45dfae1d8b5042b
Hygor Luciano <hygorluciano0@gmail.com>
2024-11-03 20:29:00 -0300

    tratamento de exceções em Java

---
+++ src/main/java/org/example/JavaFundamentals/exceptionHandling/ThrowingExceptions.java
@@ -0,0 +1,19 @@
+package org.example.JavaFundamentals.exceptionHandling;
+
+public class ThrowingExceptions {
+    public static void main(String[] args) {
+        validateAge(-1);
+    }
+
+    public static void validateAge(int age) throws IllegalArgumentException {
+        if (age < 0) {
+            throw new IllegalArgumentException("Age cannot be negative");
+        }
+    }
+
+     // OUT
+
+    /* Exception in thread "main" java.lang.IllegalArgumentException: Age cannot be negative
+	at org.example.JavaFundamentals.exceptionHandling.throwingExceptions.validateAge(throwingExceptions.java:10)
+	at org.example.JavaFundamentals.exceptionHandling.throwingExceptions.main(throwingExceptions.java:5) */
+}
