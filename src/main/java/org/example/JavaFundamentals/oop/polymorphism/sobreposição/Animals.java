07c195f2d31700d2f5b17c74ed694feabe2f8f81
Hygor Luciano <hygorluciano0@gmail.com>
2024-11-30 20:18:40 -0300

    Alguns exemplos de polymorphism - do tipo sobreposição

---
+++ src/main/java/org/example/JavaFundamentals/oop/polymorphism/sobreposição/Animals.java
@@ -0,0 +1,25 @@
+package org.example.JavaFundamentals.oop.polymorphism.sobreposição;
+
+public class Animals {
+    public void eat(){
+        System.out.println("animals is eating "+
+                "___________________________________");
+    }
+}
+class Dog extends Animals{
+    public void eat(){
+        System.out.println("dog is eating");
+    }
+}
+
+class Cat extends Animals{
+    public void eat(){
+        System.out.println("cat is eating");
+    }
+}
+
+class Horse extends Animals{
+    public void eat(){
+        System.out.println("horse is eating");
+    }
+}
---
+++ src/main/java/org/example/JavaFundamentals/oop/polymorphism/sobreposição/TestSobreposição.java
@@ -0,0 +1,16 @@
+package org.example.JavaFundamentals.oop.polymorphism.sobreposição;
+
+public class TestSobreposição {
+    public static void main(String[] args) {
+    Animals animal = new Animals();
+    animal.eat();
+
+    Animals animal2 = new Dog();
+    animal2.eat();
+
+    Animals cat = new Cat();
+    cat.eat();
+
+    }
+
+}
