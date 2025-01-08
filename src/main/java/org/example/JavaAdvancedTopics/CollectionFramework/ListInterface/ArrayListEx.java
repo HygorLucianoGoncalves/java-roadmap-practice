483582bbeb2e9f5296c5d1e5029372738ae30448
Hygor Luciano <hygorluciano0@gmail.com>
2025-01-07 21:11:43 -0300

    FEAT: aprendendo sobre arrayList

--- src/main/java/org/example/JavaAdvancedTopics/CollectionFramework/ListInterface/ArrayListEx.java
+++ src/main/java/org/example/JavaAdvancedTopics/CollectionFramework/ListInterface/ArrayListEx.java
@@ -1,4 +1,4 @@
-package org.example.JavaAdvancedTopics.CollectionFramework.list_interface;
+package org.example.JavaAdvancedTopics.CollectionFramework.ListInterface;
 
 import java.util.ArrayList;
 
@@ -7,14 +7,15 @@ public class ArrayListEx {
         ArrayList<String> listNames = new ArrayList<String>();//Creating arraylist
         listNames.add("Ravi");
         listNames.add("Vijay");
-        listNames.add("Ravi");
+        listNames.add("João");
         listNames.add("Ajay");
         listNames.add("aloo");
 
         listNames.remove("Ravi");//remove Ravi
         listNames.removeFirst();//remove Vijay
+        listNames.removeLast();//remove aloo
 
-        System.out.println("test .get: "+listNames.get(0));
+        System.out.println("test .get: " + listNames.get(0));
 
         listNames.forEach(System.out::println);
 
